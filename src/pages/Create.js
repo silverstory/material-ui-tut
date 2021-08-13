d70f736172541763eaaec8e07577f5d8aeb00bcc
Sachu <april.medina2011@gmail.com>
2021-08-13 16:13:23 +0800

    Finished lesson 3

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        onClick={() => {
+          console.log('you clicked me')
+        }}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+
+      {/* <Button type="submit">Submit</Button>
+      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
+
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+
+    </Container>
   )
 }
